278711cda571f8a0a99e11ae5925ca002b8a7ad5
Manikanta Itikela <113256332+Manikanta79i@users.noreply.github.com>
2023-04-10 13:10:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-//your code here
+ const bands = ["The Beatles", "Adele", "The Rolling Stones", "Elton John", "Nirvana", "Led Zeppelin", "The Who", "Pink Floyd"];
+
+      // function to strip articles from band names
+      function strip(bandName) {
+        return bandName.replace(/^(a |an |the )/i, "").trim();
+      }
+
+      // sort the band names and display them in ul list
+      const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1);
+      document.querySelector("#bands").innerHTML = sortedBands.map(band => `<li>${band}</li>`).join("");
 
